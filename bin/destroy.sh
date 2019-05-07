81aa4a6bf0beab6e0bd4a4c635fbeed01f6206e8
Mevan Samaratunga <mevansam@gmail.com>
2019-05-07 22:17:08 +0300

    fixed interpolation failure

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -6,7 +6,13 @@ for d in $(bosh deployments | awk '/bosh-/{ print $1 }'); do
   bosh -d $d delete-deployment --force
 done
 
+key_file=""
+if [[ -e ${keys_path}/pcf.pem ]]; then
+  key_file="--var-file=private_key=${keys_path}/pcf.pem"
+fi
+
 bosh delete-env \
+  $key_file \
   --state .state/cp-state.json \
   --vars-store .state/cp-creds.yml \
   .state/cp-manifest.yml
