c946f17ad4a804b61a42430fd2597ea8514a2fe9
Mevan Samaratunga <mevansam@gmail.com>
2019-05-07 22:10:28 +0300

    delete deployments

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+for d in $(bosh deployments | awk '/bosh-/{ print $1 }'); do 
+  bosh -d $d delete-deployment --force
+done
+
 bosh delete-env \
   --state .state/cp-state.json \
   --vars-store .state/cp-creds.yml \
