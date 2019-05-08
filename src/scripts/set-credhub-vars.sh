95cc12cb4ea71b30fe44654bc505b043e08adddc
Mevan Samaratunga <mevansam@gmail.com>
2019-05-08 14:59:34 +0300

    adding pipeline

--- src/scripts/set-credhub-vars.sh
+++ src/scripts/set-credhub-vars.sh
@@ -37,27 +37,41 @@ if [[ $? -eq 0 ]]; then
     credhub set -n "/cp/credhub_client_secret" -t password \
       -w "$(bosh interpolate --no-color $creds_path --path /credhub_admin_client_secret)"
 
+    credhub set -n "/cp/s3_url" -t value \
+      -v "http://$(bosh interpolate ${root_dir}/vars.yml --path /minio_host):9000"
+
     credhub set -n "/cp/concourse_client_id" -t value \
       -v "concourse"
     credhub set -n "/cp/concourse_client_secret" -t password \
       -w "$(bosh interpolate --no-color $creds_path --path /concourse_client_secret)"
-
-    credhub set -n "/pcf/ops_manager_host" -t value \
-      -v "$(bosh interpolate ${root_dir}/vars.yml --path /ops_manager_host)"
-    credhub set -n "/pcf/ops_manager_user" -t value \
-      -v "$(bosh interpolate ${root_dir}/vars.yml --path /ops_manager_user)"
-    credhub set -n "/pcf/ops_manager_password" -t password \
-      -w "$(bosh interpolate ${root_dir}/vars.yml --path /ops_manager_password)"
-    credhub set -n "/pcf/ops_manager_decryption_phrase" -t password \
-      -w "$(bosh interpolate ${root_dir}/vars.yml --path /ops_manager_decryption_phrase)"
-    credhub set -n "/pcf/pas_credhub_encryption_key" -t password \
-      -w "$(bosh interpolate ${root_dir}/vars.yml --path /pas_credhub_encryption_key)"
 
     credhub set -n "/pcf/config_git_repo_url" -t value \
       -v "$automation_git_repo_path"
     credhub set -n "/pcf/config_git_repo_key" -t ssh \
       -p "$HOME/.ssh/git.pem" \
       -u "$HOME/.ssh/git.pem.pub"
+
+    # Foundation specific variables
+
+    credhub set -n "/pcf-sandbox/vcenter-ip" \
+      -v "$(bosh interpolate ${root_dir}/vars.yml --path /vcenter_ip)"
+    credhub set -n "/pcf-sandbox/vcenter-user" \
+      -v "$(bosh interpolate ${root_dir}/vars.yml --path /vcenter_user)"
+    credhub set -n "/pcf-sandbox/vcenter-password" \
+      -v "$(bosh interpolate ${root_dir}/vars.yml --path /vcenter_password)"
+
+    credhub set -n "/pcf-sandbox/opsman-host" \
+      -v "$(bosh interpolate ${root_dir}/vars.yml --path /opsman_sandbox_host)"
+    credhub set -n "/pcf-sandbox/opsman-user" \
+      -v "$(bosh interpolate ${root_dir}/vars.yml --path /opsman_sandbox_user)"
+    credhub set -n "/pcf-sandbox/opsman-password" \
+      -v "$(bosh interpolate ${root_dir}/vars.yml --path /opsman_sandbox_password)"
+    credhub set -n "/pcf-sandbox/opsman-decryption_phrase" \
+      -v "$(bosh interpolate ${root_dir}/vars.yml --path /opsman_sandbox_decryption_phrase)"
+    credhub set -n "/pcf-sandbox/opsman-ssh-password" \
+      -v "$(bosh interpolate ${root_dir}/vars.yml --path /opsman_sandbox_ssh_password)"
+    credhub set -n "/pcf-sandbox/pas-credhub-encryption_key" -t password \
+      -w "$(bosh interpolate ${root_dir}/vars.yml --path /pas_credhub_encryption_key)"
 
     (grep "^creds_sha1=" .state/checksums 2>&1 >/dev/null \
         && sed -i "s|^creds_sha1=.*|creds_sha1=${updated_creds_sha1}|" .state/checksums) \
