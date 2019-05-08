7190dedae9464a01ab88e800dea169b64a084265
Mevan Samaratunga <mevansam@gmail.com>
2019-05-09 00:11:10 +0300

    removed hardcoded creds

--- src/scripts/set-credhub-vars.sh
+++ src/scripts/set-credhub-vars.sh
@@ -49,46 +49,6 @@ if [[ $? -eq 0 ]]; then
       -p "$HOME/.ssh/git.pem" \
       -u "$HOME/.ssh/git.pem.pub"
 
-    # Foundation specific variables
-
-    credhub set -n "/pcf-sandbox/vcenter-ip" -t value \
-      -v "$(bosh interpolate ${root_dir}/vars.yml --path /vcenter_ip)"
-    credhub set -n "/pcf-sandbox/vcenter-user" -t value \
-      -v "$(bosh interpolate ${root_dir}/vars.yml --path /vcenter_user)"
-    credhub set -n "/pcf-sandbox/vcenter-password" -t password \
-      -w "$(bosh interpolate ${root_dir}/vars.yml --path /vcenter_password)"
-
-    credhub set -n "/pcf-sandbox/opsman-host" -t value \
-      -v "$(bosh interpolate ${root_dir}/vars.yml --path /opsman_sandbox_host)"
-    credhub set -n "/pcf-sandbox/opsman-user" -t value \
-      -v "$(bosh interpolate ${root_dir}/vars.yml --path /opsman_sandbox_user)"
-    credhub set -n "/pcf-sandbox/opsman-password" -t password \
-      -w "$(bosh interpolate ${root_dir}/vars.yml --path /opsman_sandbox_password)"
-    credhub set -n "/pcf-sandbox/opsman-decryption_phrase" -t password \
-      -w "$(bosh interpolate ${root_dir}/vars.yml --path /opsman_sandbox_decryption_phrase)"
-    credhub set -n "/pcf-sandbox/opsman-ssh-password" -t password \
-      -w "$(bosh interpolate ${root_dir}/vars.yml --path /opsman_sandbox_ssh_password)"
-    credhub set -n "/pcf-sandbox/pas-credhub-encryption_key" -t password \
-      -w "$(bosh interpolate ${root_dir}/vars.yml --path /pas_credhub_encryption_key)"
-
-    credhub set -n "/concourse/main/deploy-sandbox/default_ca" -t certificate \
-      -r "$(bosh interpolate --no-color $creds_path --path /default_ca/ca)" \
-      -c "$(bosh interpolate --no-color $creds_path --path /default_ca/certificate)" \
-      -p "$(bosh interpolate --no-color $creds_path --path /default_ca/private_key)"
-      
-    credhub set -n "/concourse/main/deploy-sandbox/credhub_url" -t value \
-      -v "https://$(bosh interpolate ${root_dir}/vars.yml --path /dns_name):8844"
-    credhub set -n "/concourse/main/deploy-sandbox/credhub_client_id" -t value \
-      -v "credhub-admin"
-    credhub set -n "/concourse/main/deploy-sandbox/credhub_client_secret" -t password \
-      -w "$(bosh interpolate --no-color $creds_path --path /credhub_admin_client_secret)"
-
-    credhub set -n "/concourse/main/deploy-sandbox/config_git_repo_url" -t value \
-      -v "$automation_git_repo_path"
-    credhub set -n "/concourse/main/deploy-sandbox/config_git_repo_key" -t ssh \
-      -p "$HOME/.ssh/git.pem" \
-      -u "$HOME/.ssh/git.pem.pub"
-
     (grep "^creds_sha1=" .state/checksums 2>&1 >/dev/null \
         && sed -i "s|^creds_sha1=.*|creds_sha1=${updated_creds_sha1}|" .state/checksums) \
     || echo -e "creds_sha1=${updated_creds_sha1}" >> .state/checksums
