450686ed0da9fd6a91c2ad2b779341a9745b2958
Mevan Samaratunga <mevansam@gmail.com>
2019-05-09 10:43:25 +0300

    set credhub credentials

--- src/scripts/set-foundation-creds.sh
+++ src/scripts/set-foundation-creds.sh
@@ -4,15 +4,24 @@ set -eux
 
 if [[ $set_foundation_creds == yes ]]; then
 
+  default_ca=$(bosh interpolate --no-color $creds_path --path /default_ca/ca)
+
   s3_host=$(bosh interpolate ${root_dir}/vars.yml --path /minio_host)
   s3_accesskey=$(credhub get -n /cp/s3_accesskey -q)
   s3_secretkey=$(credhub get -n /cp/s3_secretkey -q)
+
+  credhub_url=$(credhub get -n /cp/credhub_url -q)
+  credhub_client_id=$(credhub get -n /cp/credhub_client_id -q)
+  credhub_client_secret=$(credhub get -n /cp/credhub_client_secret -q)
 
   for i in $(seq 0 $((num_foundations-1))); do
     name=$(bosh interpolate ${root_dir}/vars.yml --path /foundations/$i/name)
 
     credhub set -n "/concourse/main/deploy-${name}/foundation_name" \
       -t value -v "$name"
+
+    credhub set -n "/concourse/main/deploy-${name}/default_ca" 
+      -t value -v "$default_ca"
 
     credhub set -n "/concourse/main/deploy-${name}/config_git_repo_url" -t value \
       -v "$automation_git_repo_path"
@@ -26,12 +35,12 @@ if [[ $set_foundation_creds == yes ]]; then
     credhub set -n "/concourse/main/deploy-${name}/s3_secretkey"\
       -t password -w "$s3_secretkey"
 
-    credhub set -n "/concourse/main/deploy-${name}/s3_url" 
-      -t value -v "http://${s3_host}:9000"
-    credhub set -n "/concourse/main/deploy-${name}/s3_url" 
-      -t value -v "http://${s3_host}:9000"
-    credhub set -n "/concourse/main/deploy-${name}/s3_url" 
-      -t value -v "http://${s3_host}:9000"
+    credhub set -n "/concourse/main/deploy-${name}/credhub_url" 
+      -t value -v "$credhub_url"
+    credhub set -n "/concourse/main/deploy-${name}/credhub_client_id" 
+      -t value -v "$credhub_client_id"
+    credhub set -n "/concourse/main/deploy-${name}/credhub_client_secret" 
+      -t value -v "$credhub_client_secret"
 
     opsman_host=$(bosh interpolate ${root_dir}/vars.yml --path /foundations/$i/opsman_host)
     opsman_user=$(bosh interpolate ${root_dir}/vars.yml --path /foundations/$i/opsman_user)
