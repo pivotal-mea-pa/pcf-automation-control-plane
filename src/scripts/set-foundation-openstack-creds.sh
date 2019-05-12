023c79d997dcc8c32d8d4c10248b681a6f954a20
Mevan Samaratunga <mevansam@gmail.com>
2019-05-12 09:33:37 +0400

    added directory config template

--- src/scripts/set-foundation-openstack-creds.sh
+++ src/scripts/set-foundation-openstack-creds.sh
@@ -8,18 +8,21 @@ openstack_project=$(bosh interpolate ${root_dir}/vars.yml --path /openstack_proj
 openstack_username=$(bosh interpolate ${root_dir}/vars.yml --path /openstack_username)
 openstack_password=$(bosh interpolate ${root_dir}/vars.yml --path /openstack_password)
 
-credhub set -n "/concourse/main/deploy-${name}/auth_url" 
+credhub set -n "/concourse/main/deploy-${name}/auth_url" \
   -t value -v "$auth_url"
-credhub set -n "/concourse/main/deploy-${name}/openstack_domain" 
+credhub set -n "/concourse/main/deploy-${name}/openstack_domain" \
   -t value -v "$openstack_domain"
-credhub set -n "/concourse/main/deploy-${name}/openstack_project" 
+credhub set -n "/concourse/main/deploy-${name}/openstack_project" \
   -t value -v "$openstack_project"
-credhub set -n "/concourse/main/deploy-${name}/openstack_username" 
+credhub set -n "/concourse/main/deploy-${name}/openstack_username" \
   -t value -v "$openstack_username"
-credhub set -n "/concourse/main/deploy-${name}/openstack_password" 
+credhub set -n "/concourse/main/deploy-${name}/openstack_password" \
   -t password -w "$openstack_password"
 
-credhub set -n "/concourse/main/deploy-${name}/opsman_instance_name" 
+credhub set -n "/concourse/main/deploy-${name}/openstack_ssh_private_key" \
+  -t ssh -p "${keys_path}/pcf.pem"
+
+credhub set -n "/concourse/main/deploy-${name}/opsman_instance_name" \
   -t password -w "pcf-opsman-${name}"
 
 credhub set -n "/concourse/main/deploy-${name}/opsman_image_s3_versioned_regexp" \
