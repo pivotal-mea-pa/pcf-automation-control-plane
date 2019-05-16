d45812a04dc56ad34fa05c665c1a10907e5fcd93
Mevan Samaratunga <mevansam@gmail.com>
2019-05-16 17:54:48 +0400

    create default buckets in minio

--- src/scripts/deploy-minio.sh
+++ src/scripts/deploy-minio.sh
@@ -21,4 +21,15 @@ if [[ $action != create-manifests-only \
 
   set -e
   bosh -n -d $minio_name deploy $minio_manifest
+
+  s3_host=$(bosh interpolate ${root_dir}/vars.yml --path /minio_host)
+  s3_accesskey=$(credhub get -q -n '/cp/s3_accesskey')
+  s3_secretkey=$(credhub get -q -n '/cp/s3_secretkey')
+
+  mc config host add auto http://$s3_host:9000 $s3_accesskey $s3_secretkey
+
+  [[ $(mc ls auto/ | awk '/pivnet-products\/$/{ print $5 }') == pivnet-products/ ]] || \
+    mc mb auto/pivnet-products
+  [[ $(mc ls auto/ | awk '/pcf-exports\/$/{ print $5 }') == pcf-exports/ ]] || \
+    mc mb auto/pcf-exports
 fi
