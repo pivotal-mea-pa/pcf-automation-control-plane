9ddb0cdf171ac4a0bd46b7aedbb5dba9bac820d5
Mevan Samaratunga <mevansam@gmail.com>
2019-05-24 22:58:37 +0400

    generic download script

---
+++ src/stemcells/scripts/windows/download.ps1
@@ -0,0 +1,34 @@
+Param(
+  [string][Parameter(Mandatory=$True)]$Message,
+  [string][Parameter(Mandatory=$True)]$DownloadURL,
+  [string][Parameter(Mandatory=$True)]$OutputFile
+)
+
+$DownloadPath = "C:\Stemcell-Build\Downloads"
+
+Start-Transcript -path "C:\Stemcell-Build\Logs\build.log" -append
+$ErrorActionPreference = "Stop"
+
+# Wait for internet connectivity
+Do {
+  Write-Host "Waiting for internet connectivity..."
+  try {
+    $HTTP_Request = [System.Net.WebRequest]::Create('http://google.com')
+    $HTTP_Response = $HTTP_Request.GetResponse()
+    $HTTP_Status = [int]$HTTP_Response.StatusCode
+    $HTTP_Response.Close()
+  } catch {
+    $HTTP_Status = 500
+  }
+} Until ($HTTP_Status -eq 200)
+
+# Download file
+Write-Output @"
+$Message
+  <== $DownloadURL
+  ==> $DownloadPath\$OutputFile
+"@
+Invoke-WebRequest `
+  -uri "$DownloadURL" `
+  -outfile "$DownloadPath\$OutputFile"
+Unblock-File "$DownloadPath\$OutputFile"
