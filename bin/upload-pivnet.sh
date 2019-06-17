3892c822bc480510b67cfff3020c15e2453e2bf1
Mevan Samaratunga <mevansam@gmail.com>
2019-06-17 15:00:10 +0000

    updated files can be overwritten

--- bin/upload-pivnet.sh
+++ bin/upload-pivnet.sh
@@ -11,4 +11,4 @@ s3_accesskey=$(credhub get -n /cp/s3_accesskey -q)
 s3_secretkey=$(credhub get -n /cp/s3_secretkey -q)
 
 mc config host add auto http://${s3_host}:9000 $s3_accesskey $s3_secretkey
-mc mirror $pivnet_download_dir auto/pivnet-products/
+mc mirror --overwrite $pivnet_download_dir auto/pivnet-products/
