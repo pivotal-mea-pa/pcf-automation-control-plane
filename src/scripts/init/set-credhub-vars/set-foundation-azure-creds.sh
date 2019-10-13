9faa6c7cfb5c34367f74f70d4d07ba0463073291
hodeh <hodeh@pivotal.io>
2019-10-13 17:52:16 +0400

    azure creds

---
+++ src/scripts/init/set-credhub-vars/set-foundation-azure-creds.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -eux
+
+azure_subscription_id=$(bosh interpolate ${root_dir}/vars.yml --path /subscription_id)
+azure_tenant_id=$(bosh interpolate ${root_dir}/vars.yml --path /tenant_id)
+azure_resource_group=$(bosh interpolate ${root_dir}/vars.yml --path /resource_group_name)
+azure_client_id=$(bosh interpolate ${root_dir}/vars.yml --path /azure_client_id)
+azure_client_secret=$(bosh interpolate ${root_dir}/vars.yml --path /client_secret)
+azure_location=$(bosh interpolate ${root_dir}/vars.yml --path /location)
+azure_network_security_group=$(bosh interpolate ${root_dir}/vars.yml --path /default_security_group)
+azure_network_vpc_subnet=$(bosh interpolate ${root_dir}/vars.yml --path /vpc_subnet)
+azure_network_storage_account=$(bosh interpolate ${root_dir}/vars.yml --path /storage_account_name)
+azure_network_ssh_public_key=$(bosh interpolate ${root_dir}/vars.yml --path /ssh.public_key)
+
+# PCF automation interpolated
+credhub set -n "/pcf/${name}/azure_subscription_id" \
+  -t value -v "$azure_subscription_id"
+credhub set -n "/pcf/${name}/azure_tenant_id" \
+  -t value -v "$azure_tenant_id"
+credhub set -n "/pcf/${name}/azure_resource_group" \
+  -t password -w "$azure_resource_group"
+credhub set -n "/pcf/${name}/azure_client_id" \
+  -t value -v "$azure_client_id"
+credhub set -n "/pcf/${name}/azure_client_secret" \
+  -t value -v "$azure_client_secret"
+credhub set -n "/pcf/${name}/azure_location" \
+  -t value -v "$azure_location"
+credhub set -n "/pcf/${name}/azure_network_security_group" \
+  -t value -v "$azure_network_security_group"
+credhub set -n "/pcf/${name}/azure_network_vpc_subnet" \
+  -t value -v "$azure_network_vpc_subnet"
+credhub set -n "/pcf/${name}/azure_network_storage_account" \
+  -t value -v "$azure_network_storage_account"
+credhub set -n "/pcf/${name}/azure_network_ssh_public_key" \
+  -t value -v "$azure_network_ssh_public_key"
